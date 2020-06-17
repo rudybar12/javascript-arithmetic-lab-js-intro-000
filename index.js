63be8c2f60037dfe12710d6719183062fbe8c214
rodolfo Barrientos <rudybar12@yahoo.com>
2020-06-17 02:35:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function subtract(a, b){
   number = a - b;
   return a-b;
 }
+function multiple(a, b){
+  number = a * b;
+  return a * b;
+}
+function divide(a, b){
+  number = a / b;
+  return a / b ;
+}
+funcion 
